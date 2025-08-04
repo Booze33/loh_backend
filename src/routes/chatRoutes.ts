3c2cd494757de0e50e95ed7bd4321597f79a6b06
ttisloh <ttisloh@gmail.com>
2025-08-04 21:29:34 +0000

    update

--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -1,11 +1,12 @@
 import { Hono } from 'hono';
-import { createChatSession, getChatSessions, sendMessageToSession } from '../controllers/chatController';
+import { createChatSession, getChatMessages, getChatSessions, sendMessageToSession } from '../controllers/chatController';
 import { authMiddleware } from '../middleware/auth.ts';
 
 const chatRoutes = new Hono();
 
 chatRoutes.post('/sessions', authMiddleware, createChatSession);
 chatRoutes.get('/sessions', authMiddleware, getChatSessions);
+chatRoutes.get('/sessions/:id', authMiddleware, getChatMessages);
 chatRoutes.post('/sessions/:id/messages', authMiddleware, sendMessageToSession);
 
 export default chatRoutes;
